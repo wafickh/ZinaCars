616d2baead06912d59cce29992f2e16ae70ddef7
wafic khalife <98118662+wafickh@users.noreply.github.com>
2023-08-01 20:19:05 +0300

    Update car.js

--- backend/models/car.js
+++ backend/models/car.js
@@ -7,17 +7,14 @@ const Schema = mongoose.Schema;
 const CarSchema = new Schema({
 
     Name: { type: String, required: true },
-    Color: { type: String, required: true },
+    Color: { type: String },
     Price: { type: String, required: true },
-    Condition: { type: String },
     Year: { type: String, required: true },
     images: [{
 
         type: Object,
         required: true
     }],
-    Cylinders: { type: String, required: true },
-
 
 
 
